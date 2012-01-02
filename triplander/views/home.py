afb0ed1941c693635ecb090b1443aa36a1d8ee09
Angelo Romano <angelo@lieferheld.de>
2012-01-02 17:43:10 +0100

    additional stuff

--- triplander/views/home.py
+++ triplander/views/home.py
@@ -31,17 +31,31 @@ class CityView(BaseView):
 class AJAXSearchView(BaseView):
     mime_type = "application/json"
 
+    @staticmethod
+    def _get_html_label(name, country_code):
+        base_str = (u'<img src="/media/img/flags/country_{country_code}.png"'
+                    u' alt="Flag {country_code}" style="margin-right: 4px" />'
+                    u'{name}').format(
+                          name=name,
+                          country_code=country_code)
+        return base_str
+
     def _get_cities_by_prefix(self, prefix):
         for city in City.find_prefix('name', prefix):
+            country_code = city.relation_set.country.code.lower()
             yield {"name": city.name,
                    "slug": city.slug,
-                   "country_code": city.relation_set.country.code.lower()}
+                   "country_code": country_code,
+                   "label": self._get_html_label(city.name, country_code), 
+                   "ranking": city.total_ranking}
 
     def _get_countries_by_prefix(self, prefix):
         for country in Country.find_prefix('name', prefix):
             yield {"name": country.name,
                    "slug": country.slug,
-                   "capital_city": country.relation_set.capital_city.name}
+                   "label": self._get_html_label(country.name, country.code), 
+                   "capital_city": country.relation_set.capital_city.name,
+                   "ranking": None}
 
     @cherrypy_action(allowed_methods=["POST"], use_cache=True)
     def get_by_prefix(self, obj, prefix):
@@ -51,7 +65,7 @@ class AJAXSearchView(BaseView):
             entries = self._get_countries_by_prefix(prefix)
         
         return sorted(entries,
-                      key=lambda k: k["name"])
+                      key=lambda k: (-k["ranking"], k["name"]))
 
     @cherrypy_action(allowed_methods=["POST"], use_cache=True)
     def get_by_coordinates(self, obj, latlng):
